c109eabd241b4c0123ab2d1e3db1a7bde8d4927e
amresh.venugopal <amresh.venugopal@gmail.com>
2018-01-20 15:37:01 +0530

    mapping of nodeswrt intent & entities happens in router

--- src/map/index.js
+++
@@ -1,9 +0,0 @@
-const intentNodeMap = {
-  "greeting": "init"
-};
-
-module.exports = function nodeNameMap (intent) {
-  intent = intent.toLowerCase();
-  const nodeName = intentNodeMap[intent];
-  return nodeName || '';
-};
